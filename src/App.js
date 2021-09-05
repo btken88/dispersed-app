98cbc66581eef64b415b8196c7230b4cea8ab528
Bryce Kennedy <bryce@brycekennedy.net>
2021-09-05 14:38:47 -0600

    Adds global google analytics tag to the main app page.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import './App.css';
-import { createBrowserHistory } from 'history'
-import MapPage from './components/MapPage';
-import HomePage from './components/HomePage';
-import AboutPage from './components/AboutPage';
-import Favorites from './components/Favorites';
-import SignIn from './components/SignIn'
-import ReactGA from 'react-ga';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
+import { createBrowserHistory } from "history";
+import MapPage from "./components/MapPage";
+import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
+import Favorites from "./components/Favorites";
+import SignIn from "./components/SignIn";
 
-const history = createBrowserHistory()
+const history = createBrowserHistory();
 
 function App() {
-  ReactGA.initialize('G-JL1TXETBCN');
-  history.listen(location => {
-    ReactGA.set({ page: location.pathname }); // Update the user's current page
-    ReactGA.pageview(location.pathname); // Record a pageview for the given page
-  });
-
   return (
     <div className="App">
       <Router>
         <Switch>
-          <Route exact path='/'>
+          <Route exact path="/">
             <HomePage />
           </Route>
           <Route exact path="/map">
