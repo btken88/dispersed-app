82dc8c501f7d619ed44276aac735720c5feff8ea
Bryce Kennedy <bryce@brycekennedy.net>
2021-09-10 18:58:18 -0600

    Updates pages to reset the document title on page load

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../component-css/homePage.css'
 import { Link } from 'react-router-dom'
 
 export default function HomePage() {
+  useEffect(() => {
+    document.title = "Dispersed"
+  })
   const token = localStorage.getItem('token')
   return (
     <div className="home-page">
